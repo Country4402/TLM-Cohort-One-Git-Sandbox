847c0a9fdcfa50499325d880825331b4afac4bda
William Erickson <149959524+Country4402@users.noreply.github.com>
2023-11-26 14:25:58 -0800

    hhhmmm

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import{ createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css';
 import App from './App';
+import NavBar from './components/NavBar/NavBar'
+import 'bootstrap/dist/js/bootstrap.js'
+import 'bootstrap/dist/css/bootstrap.css'
+
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <><NavBar /><App /></>
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(<RouterProvider router={router} />)
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
